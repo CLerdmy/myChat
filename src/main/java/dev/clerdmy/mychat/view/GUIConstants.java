01f8cc9fc0a4569a6e627fd7b8948b05d76ce3b8
CLerdmy <podturov@gmail.com>
2025-04-14 19:55:49 +0300

    update

--- src/main/java/dev/clerdmy/mychat/view/GUIConstants.java
+++ src/main/java/dev/clerdmy/mychat/view/GUIConstants.java
@@ -4,16 +4,24 @@ import java.awt.*;
 
 public class GUIConstants {
 
-    public static final int ANGLE = 40;
+    public static final int ANGLE = 30;
+
+    public static final int MIN_WIDTH = 400;
+    public static final int MIN_HEIGHT = 600;
+    public static final int DEFAULT_WIDTH = 600;
+    public static final int DEFAULT_HEIGHT = 600;
+    public static final int SQUARE = 40;
 
     public static final Font MAIN_FONT = new Font("Open Sans", Font.PLAIN, 14);
     public static final Font MAIN_FONT_BOLD = new Font("Open Sans", Font.BOLD, 14);
+    public static final Font MAIN_FONT_BOLD_LARGE = new Font("Open Sans", Font.BOLD, 20);
     public static final Font TEST_FONT = new Font("Open Sans", Font.BOLD, 28);
 
     public static final Color WHITE = new Color(255, 255, 255);
     public static final Color GRAY = new Color(30, 30, 30);
     public static final Color LIGHTER_GRAY = new Color(157, 157, 157);
     public static final Color BLACK = new Color(15, 15, 15);
+    public static final Color ANOTHER_BLACK = new Color(20, 20, 20);
     public static final Color LIGHTER_BLACK = new Color(50, 50, 50);
 
 }
